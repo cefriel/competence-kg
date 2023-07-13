6c469dc70d2b3ccc30565a1ef259db09cadfb52b
Mario Scrocca <mario.scrocca@gmail.com>
2023-07-13 13:10:13 +0200

    Init model and generate/load procedures

---
+++ script/load.py
@@ -0,0 +1,118 @@
+# tag::import[]
+# Import the neo4j dependency
+from neo4j import GraphDatabase
+# end::import[]
+
+
+uri = "neo4j://localhost:7687"
+
+"""
+Example Authentication token.
+You can pass the username and password as a tuple.
+"""
+username = 'neo4j'
+password = 'kgCefriel'
+
+# tag::auth[]
+auth = (username, password)
+# end::auth[]
+
+# tag::driver[]
+# Create a new Driver instance
+driver = GraphDatabase.driver(uri,auth=auth)
+# end::driver[]
+
+# tag::verifyConnectivity[]
+# Verify the connection details
+driver.verify_connectivity()
+# end::verifyConnectivity[]
+
+from os import listdir
+from os.path import abspath, basename, isfile, join, dirname
+
+local_folder = dirname(abspath(__file__))
+import_path = join(local_folder, "../data")
+
+# tag::driver.session[]
+with driver.session() as session:
+# end::driver.session[]
+
+    def create_constraint_competences(tx):
+        return tx.run("""
+            CREATE CONSTRAINT CompetenceId IF NOT EXISTS ON (c:Competence) ASSERT c.competenceId IS UNIQUE
+            """)
+    def create_constraint_users(tx):
+        return tx.run("""
+            CREATE CONSTRAINT EmployeeId IF NOT EXISTS ON (e:Employee) ASSERT e.id IS UNIQUE
+            """)
+    print("Create constraints")
+    session.execute_write(create_constraint_competences)
+    session.execute_write(create_constraint_users)
+
+    def create_competences(tx):
+        return tx.run("""
+            LOAD CSV WITH HEADERS
+            FROM "file:///digital_competences.csv"
+            AS row
+            MERGE (c:Competence {competenceId: row.id})
+            ON CREATE SET
+            c.dimension = row.dimension,
+            c.source = row.source,
+            c.competence = row.competence,
+            c.description = row.description
+            """)
+    print("Create competences")
+    session.execute_write(create_competences)
+
+    def create_units(tx):
+        return tx.run("""
+            LOAD CSV WITH HEADERS
+            FROM "file:///employees.csv"
+            AS row
+            MERGE (u:Unit {unitName: row.unit})
+            """)
+    print("Create units")
+    session.execute_write(create_units)
+
+    def create_employees(tx):
+        return tx.run("""
+            LOAD CSV WITH HEADERS
+            FROM "file:///employees.csv"
+            AS row
+            MERGE (e:Employee {employeeId: row.id})
+            ON CREATE SET
+            e.name = row.name,
+            e.surname = row.surname
+            MATCH (u:Unit {unitName: row.unit})
+            MERGE (e)-[r:IN_UNIT]->(u)
+            """)
+    print("Create employees")
+    session.execute_write(create_employees)
+
+    def load_assignments(tx, filename, user, level, weight):
+        cypher = """
+            LOAD CSV WITH HEADERS
+            FROM "file:///assignment.csv"
+            AS row
+            MATCH (e:Employee {{employeeId: row.employee_id}})
+            MATCH (c:Competence {{competenceId: row.competence_id}})
+            MERGE (e)-[r:HAS_COMPETENCE]->(c)
+            ON CREATE SET r.level = row.level
+        """
+        return tx.run(cypher)
+    session.execute_write(load_assignments)
+    
+    def load_interest(tx, filename, user, level, weight):
+        cypher = """
+            LOAD CSV WITH HEADERS
+            FROM "file:///assignment.csv"
+            AS row
+            WITH row WHERE rox.interest = 1
+            MATCH (e:Employee {{employeeId: row.employee_id}})
+            MATCH (c:Competence {{competenceId: row.competence_id}})
+            MERGE (e)-[r:INTERESTED_IN]->(c)
+        """
+        return tx.run(cypher)
+    session.execute_write(load_interest)
+
+driver.close()
